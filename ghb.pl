4dba68b48aff0d74936ed2addc8f321dbbf4ebc7
faraco <skelic3@gmail.com>
2018-01-07 05:39:08 +0800

    Tidy up ghb.pl

--- ghb.pl
+++ ghb.pl
@@ -4,7 +4,6 @@
 use strict;
 use warnings;
 
-use Data::Dumper 'Dumper';
 use JSON::MaybeXS 'decode_json';
 use Pithub;
 use v5.10;
@@ -12,7 +11,7 @@ use v5.10;
 my $repos = Pithub::Repos->new;
 my $result = $repos->list(user => 'faraco');
 
-# This node apparently are not changed to Perl data structure.
+# This node apparently is not converted to Perl data structure.
 # So, we decode the node and make the git_url easily accessible.
 my $decoded_json = decode_json $result->{response}{_content};
 
@@ -21,5 +20,7 @@ foreach my $content_node (@{$decoded_json})
     system("git clone $content_node->{git_url}");
 }
 
-# Create tar.gz and put every cloned repository directories inside it (including ghb.pl script itself).
-system('tar -czvf /tmp/faraco-github-backup.tar.gz .; mv /tmp/faraco-github-backup.tar.gz .');
+# Create faraco-github-backup.tar.gz and put every cloned repository directories inside it (including ghb.pl script itself).
+system(
+    'tar -czvf /tmp/faraco-github-backup.tar.gz .; mv /tmp/faraco-github-backup.tar.gz .'
+);
