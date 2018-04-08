5ab0ac8e64b8cfb913a2f27ecdf98c7bc1603151
faraco <skelic3@gmail.com>
2018-04-09 00:58:39 +0800

    Update pytempl.sh to be 'git' oriented.

--- pytempl.sh
+++ pytempl.sh
@@ -10,6 +10,11 @@ readme_tpl.py "$name" "$license"
 echo '#!/usr/bin/env python3' > main.py
 mkdir tests src
 
+echo '__pycache__' >> .gitignore
+echo '*.pyc' >> .gitignore
+
+git init
+
 echo "$name project template under the $license were successfuly built"
 
 
