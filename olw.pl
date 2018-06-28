639d170d1f09bf885a234d87d8e320002a604fcc
faraco <skelic3@gmail.com>
2018-06-28 11:57:03 +0800

    Refactored for maintainability

--- olw.pl
+++ olw.pl
@@ -1,75 +1,127 @@
-#!/usr/bin/env perl
+#source /etc/profile.d/rvm.sh!/usr/bin/env perl
 
 use strict;
 use warnings;
+use v5.10;
+
+use open qw(:std :utf8);
 use autodie;
 use DateTime;
+use File::Temp 'tempfile';
 
-print "What is your article number?: ";
-my $number = <STDIN>;
-die "Must be a number!" if $number !~ /[0-9]+/;
-chomp $number;
-
-print "Write your post/article title: ";
-my $title = <STDIN>;
-chomp $title;
-
-my $title_in_article = ucfirst $title;
-
-print "Write your article content using vim? ";
-<STDIN>;
-
-my $content;
-my $file_to_read = int(rand(100000)) . "-olw.txt";
-system("vim $file_to_read");
-
-open(my $fr, '<', $file_to_read);
+# prompt for file inputs
+sub prompt_file
 {
-    local $/;
-    $content = <$fr>;
+    my $number;
+    my $title;
+    my $content;
+
+    print "Article number: ";
+    chomp($number = <STDIN>);
+
+    if (!defined($number) || $number !~ /^[0-9]+$/)
+    {
+        die "Article number cannot be empty and it must be a whole number!\n";
+    }
+
+    print "Article title: ";
+    chomp($title = <STDIN>);
+
+    if (!defined($title) || length($title) < 3)
+    {
+        die "Article title cannot be less than 3 characters or empty!\n";
+    }
+
+    # use vim by default for advanced editing
+    # in command line
+    my ($fh, $tempfile) = tempfile;
+    my $vim_proc = "vim " . $tempfile;
+    system($vim_proc);
+
+    open(my $file, '<', $tempfile);
+
+    # load everything from tempfile into the
+    # content variable
+    {
+        local $/;
+        $content = <$fh>;
+    }
+
+    chomp($content);
+    my %result = (
+                  number  => $number,
+                  title   => $title,
+                  content => $content
+                 );
+
+    return \%result;
 }
-close $fr;
-unlink $file_to_read;
 
-my $dt = DateTime->now;
-my $written_time = $dt->dmy;
+sub to_html
+{
+    my ($title, $content) = @_;
 
-my $html_template = <<"END_TEMPLATE";
+    my $written_time = DateTime->now->dmy;
+
+    my $template = <<"    END_TEMPLATE";
 <!DOCTYPE html>
-<html lang="en">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<html lang="en"/>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-with, initial-scale=1.0"/>
+        
+        <title>$title</title>
+    </head>
 
-    <title>$title_in_article</title>
-    
     <body>
-    <h1>faraco's personal site</h1>
-    <p>
-    <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="https://twitter.com/_faraco">faraco's Twitter</a></li>
-        <li><a href="https://github.com/faraco">faraco's Github</a></li>
-    </ul>
-    </p>
-    
-    <hr>
-    
-    <h1>$title_in_article</h1>
-    <h2>$written_time</h2>
-    <p>$content</p>
-    
-    <hr>
-    <p style="text-align: center;">Made with simplicity by <b>faraco</b></p>
-    <hr>
+        <h1>faraco's Personal Site</h1>
+        <p>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="https://twitter.com/_faraco"Twitter</a></li>
+            <li><a href="https://gitlab.com/faraco-dev">Gitlab</a></li>
+            <li><a href="https://github.com/faraco">Github</a></li>
+        </ul>
+        </p>
+
+        <hr>
+
+        <p style="text-align: center;">Made with love by <b>faraco</b></p>
+        
+        <hr>
+        <h1>$title</h1>
+        <h2>$written_time</h2>
+        <p>$content</p>
+
+        <hr>
     </body>
 </html>
-END_TEMPLATE
+    END_TEMPLATE
 
-$title =~ tr/\'\"!@#$%^&*()-+[]{}|;:,.\///d;
-$title =~ s/ /-/g;
+    return $template;
+}
 
-my $title_for_file = lc $title;
+sub write_to_file
+{
+    my $result = shift;
 
-open(my $fh, ">", $number . "-$title_for_file.html");
-print $fh $html_template;
-close $fh;
+    my $number  = $result->{number};
+    my $title   = $result->{title};
+    my $content = $result->{content};
+
+    # I don't want the file to be clean and
+    # easy to find, so I remove all symbols
+    # and replace all whitespaces with dash '-'.
+    $title =~ tr/\'\"!\?@#$%^&*()-+[]{}|;:,\.\///d;
+    $title =~ s/ /-/g;
+
+    open(my $fh, '>', $number . "-$title.html");
+    print($fh to_html($title, $content));
+
+    say("$number-$title.html has been successfully created");
+
+    return;
+}
+
+write_to_file(prompt_file);
+
