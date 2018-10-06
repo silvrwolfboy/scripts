ebc103dcf4ca33b11eaba85585ee1b9e1b62bfa5
faraco <skelic3@gmail.com>
2018-10-06 22:05:41 +0800

    add bsd3 notice option

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Programs Descriptions
 
 * df-alert.sh - Alert user if /dev/sda1 storage size exceeds 60%.
 
-* cap.py - Append GPL3 license notice to the top of a non-blank existing file.
+* cap.py - Append Gpl-3 or bsd3 license notice to the top of a non-blank existing file.
 
 * ghb.py - Backup all of my Github's public repositories to local computer.
 
--- cap.py
+++ cap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Append GPL3 license notice into the top of ian existing file.
+# Append GPL3 or bsd3 license notice into the top of ian existing file.
 # Note: If you just trying to append to an empty file, just use
 # echo "data" | cat >> empty_file.txt
 # Usage:
@@ -8,7 +8,7 @@
 #
 
 import sys
-gpl_license = '''/*    -Insert project name and what it does-
+gpl3_license = '''/*    -Insert project name and what it does-
     Copyright (C) 2018 faraco <skelic3@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
@@ -27,8 +27,31 @@ gpl_license = '''/*    -Insert project name and what it does-
 */
 '''
 
-with open(sys.argv[1], 'r+') as f:
-    content = f.read()
-    f.seek(0, 0)
-    f.write(gpl_license.rstrip('\r\n') + '\n' + content)
+bsd3_license = '''/*
+        Copyright <YEAR> <COPYRIGHT HOLDER>
+
+        Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/'''
+
+if sys.argv[2] == 'bsd3' or sys.argv[2] == 'bsd':
+    with open(sys.argv[1], 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(bsd3_license.rstrip('\r\n') + '\n' + content)
+
+elif sys.argv[2] == 'gpl3' or sys.argv[2] == 'gpl':
+    with open(sys.argv[1], 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(gpl3_license.rstrip('\r\n') + '\n' + content)
+
 
