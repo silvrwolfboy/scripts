9ece1251f260ea1ecadacf1e243405c731189e71
faraco <skelic3@gmail.com>
2018-11-10 00:02:13 +0800

    fix typo

--- chars.sh
+++ chars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Generate random strings
-# By default, if not arguments were supplied than generate 15 chars.
+# By default, if no arguments were supplied than generate 15 chars.
 date +%s | sha256sum| base64 | head -c ${1-15}; echo
