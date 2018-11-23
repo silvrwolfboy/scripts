2c9c2f66674b5ea26efce90da74420b58ba7b2da
faraco <skelic3@gmail.com>
2018-11-23 09:38:43 +0800

    automate apt installs

--- install-rvm-ruby.sh
+++ install-rvm-ruby.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 sudo add-apt-repository ppa:rael-gc/rvm
-sudo apt-get update
-sudo apt-get install rvm
+sudo apt-get update -y
+sudo apt-get install rvm -y
 
 source /etc/profile.d/rvm.sh
 rvm install ruby
-
-source ~/.zshrc
 
 gem install bundler rake rspec wirble sinatra mysql2 cucumber rails --no-doc --no-ri
