ed7b32bedb6cc992ec88afeb99b0f1b6bfb64832
momozor <skelic3@gmail.com>
2019-08-20 02:58:48 +0800

    add file check

--- mkreadme.sh
+++ mkreadme.sh
@@ -4,10 +4,15 @@ choose_license ()
 {
     license="$1"
     gpl3_path="/usr/share/common-licenses/GPL-3"
+    mit_path="/usr/share/common-licenses/MITz"
 
     case "$license" in
-        "gpl3") current_license="$gpl3_path"
-               ;;
+        "gpl3") test -f "$gpl3_path" && \
+                current_license="$gpl3_path"
+                ;;
+        "mit")  test -f "$mit_path" && \
+                current_license="$mit_path"
+                ;;
     esac
 }
 
@@ -19,6 +24,8 @@ then
 else
         case "$project_license" in
                 "gpl3"|"GPL3"|"gpl-3") choose_license "gpl3"
+                        ;;
+                "mit"|"Mit"|"MIT") choose_license "mit"
                         ;;
                 *)
                         echo "Project license is not recognized." >&2
@@ -47,10 +54,19 @@ echo "" >> README.md
 echo "## License" >> README.md
 echo "" >> README.md
 
-case "$license" in
-    "gpl3")
+case "$project_license" in
+    "gpl3"|"GPL3"|"GPL-3"|"GPL-3.0"|"gpl-3.0"|"gpl-3")
         readme_license="GPL-3.0"
         type=" and free"
+        ;;
+    "mit"|"Mit"|"MIT")
+       readme_license="MIT"
+       type=""
+       ;;
+    *)
+       readme_license="GPL-3.0"
+       type=" and free"
+       ;;
 esac
 
 echo "This project is an open source$type software." >> README.md
